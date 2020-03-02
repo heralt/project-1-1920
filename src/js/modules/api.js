28101a2e191665ec400a2e435cced5a763927e9b
heralt <heralt@live.nl>
2020-03-02 15:20:45 +0100

    added routie

--- src/js/modules/api.js
+++ src/js/modules/api.js
@@ -1,21 +1,20 @@
 
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const endpoint = 'https://zoeken.oba.nl/api/v1/search/?q=';
-const query = 'tolkien';
+const query = 'economics';
 const key = '1e19898c87464e239192c8bfe422f280';
-const secret = '4289fec4e962a33118340c888699438d';
+//const secret = '4289fec4e962a33118340c888699438d';
 const detail = 'Default';
 const url = `${cors}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
 
-const config = {
-    Authorization: `Bearer ${secret}`
-};
+
 export function getData() {
-    return fetch(url, config)
+    return fetch(url)
         .then(response => {
             return response.json();
         })
             .then(data => {
+                console.log(data);
                 return data;
             })
         .catch(err => {
--- src/js/script.js
+++ src/js/script.js
@@ -5,25 +5,30 @@ import {getData} from "./modules/api.js";
 const main = document.querySelector('main');
 
 getData().then( json => {
-    console.log(json);
     render(json);
 });
 
 function render(data) {
     const results = data.results;
-    console.dir(results);
+    //console.dir(results);
     results.forEach((item) => {
         const html = `
             <article>
               <h2>${item.titles[0]}</h2>
               <p>${item.summaries ? item.summaries[0] : 'Geen samenvatting'}</p>
-              <img src="${
+              <a href="#boek/${item.id}"><img src="${
             item.coverimages ? item.coverimages[1] : 'Geen samenvatting'
-        }">
+        }"></a>.
             </article>
           `;
         main.insertAdjacentHTML('beforeend', html);
     });
 }
+
+routie({
+    "boek/:id": id => {
+        console.log(id);
+    },
+});
 
 
