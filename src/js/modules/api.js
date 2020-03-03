3a4e4d8515068c3296e1e696fec090efb56ab3a4
heralt <heralt@live.nl>
2020-03-03 11:08:33 +0100

    seperate endpoints

--- src/js/modules/api.js
+++ src/js/modules/api.js
@@ -1,11 +1,12 @@
 
 const cors = 'https://cors-anywhere.herokuapp.com/';
 const baseUrl = 'https://zoeken.oba.nl/api/v1';
-const endpoint = '/refine/?q=';
+const endpoint = '/search/?q=';
 const query = 'Harry';
 const key = '1e19898c87464e239192c8bfe422f280';
 const detail = 'Default';
-const url = `${cors}${baseUrl}${endpoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
+const url = `${cors}${baseUrl}${endpoint}${query}&p=jeugd&page=2&authorization=${key}&detaillevel=${detail}&output=json`;
+console.log(url);
 
 export function getData() {
     return fetch(url)
