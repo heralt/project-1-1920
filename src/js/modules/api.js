c647ca00166d37e16f8315cac745590697af72cd
heralt <heralt@live.nl>
2020-03-03 13:30:19 +0100

    fixed routie id search

--- src/js/modules/api.js
+++ src/js/modules/api.js
@@ -1,12 +1,19 @@
 
+search('school');
+
 export function search(data) {
     const cors = 'https://cors-anywhere.herokuapp.com/';
     const baseUrl = 'https://zoeken.oba.nl/api/v1';
     const endPoint = '/search/?q=';
-    const query = 'tolkien';
     const key = '1e19898c87464e239192c8bfe422f280';
     const detail = 'Default';
+    let query = '';
 
+    if(data) {
+        query = data;
+    } else {
+        query = 'kaas';
+    }
     const url = `${cors}${baseUrl}${endPoint}${query}&authorization=${key}&detaillevel=${detail}&output=json`;
     return getData(url);
 }
